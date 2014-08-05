dbadf78bb2999285a4f1a3a0d7a076b4d6aec4c8
svvashishtha <svvashishtha@gmail.com>
2014-08-05 19:20:04 +0530

    Update myscript.js
    answered on sto.
    works perfectly on jsfiddle.
    after enter on ideone and most complicated compilers.

--- data/myscript.js
+++ data/myscript.js
@@ -1,16 +1,53 @@
-//function codeboard()
-//{
+document.activeElement.addEventListener("keydown",blockInput);
 document.activeElement.addEventListener("keyup",keyc);
+
 var ch = [')','!','@','#','$','%','^','&','*','('];
 function keyc(event)
 {
-	var x = document.activeElement.value;
-	if( event.keyCode >= 48 && event.keyCode <= 57)
-		{
-			event.preventDefault();
-			x = x.substring(0, x.length - 1);
-			x= x + ch[event.keyCode -48];			
-		}
-	document.activeElement.value = x ;
+    var txtArea = document.activeElement;
+    var x = txtArea.value;
+    if( event.keyCode >= 48 && event.keyCode <= 57)
+        {
+            insertAtCaret(txtArea, ch[event.keyCode -48]);
+        }
 }
-//}
+
+function blockInput(event){
+    if( event.keyCode >= 48 && event.keyCode <= 57)
+    {
+        event.preventDefault();   
+    }
+}
+
+function insertAtCaret(txtarea,text) {
+   // var scrollPos = txtarea.scrollTop;
+    var strPos = 0;
+    var br = ((txtarea.selectionStart || txtarea.selectionStart == '0') ? 
+        "ff" : (document.selection ? "ie" : false ) );
+    if (br == "ie") { 
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        strPos = range.text.length;
+    }
+    else if (br == "ff") strPos = txtarea.selectionStart;
+
+    var front = (txtarea.value).substring(0,strPos);  
+    var back = (txtarea.value).substring(strPos,txtarea.value.length); 
+    txtarea.value=front+text+back;
+    strPos = strPos + text.length;
+    if (br == "ie") { 
+        txtarea.focus();
+        var range = document.selection.createRange();
+        range.moveStart ('character', -txtarea.value.length);
+        range.moveStart ('character', strPos);
+        range.moveEnd ('character', 0);
+        range.select();
+    }
+    else if (br == "ff") {
+        txtarea.selectionStart = strPos;
+        txtarea.selectionEnd = strPos;
+        txtarea.focus();
+    }
+    //txtarea.scrollTop = scrollPos;
+}
